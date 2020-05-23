26061978468a6547a20fe4bc808f3e315b751191
jameswilddev <jameswilddev@outlook.com>
2020-05-23 12:54:21 +0100

    Event -> EventSchema.

--- index.ts
+++ index.ts
@@ -2,64 +2,64 @@ export { default as Json } from "./json";
 export {
   createBackgroundEventSchema,
   CreateBackgroundEventSchema,
-} from "./schema/event/background/create-background-event-schema";
+} from "./schema/event-schema/background/create-background-event-schema";
 export {
   deleteBackgroundEventSchema,
   DeleteBackgroundEventSchema,
-} from "./schema/event/background/delete-background-event-schema";
+} from "./schema/event-schema/background/delete-background-event-schema";
 export {
   updateBackgroundNameEventSchema,
   UpdateBackgroundNameEventSchema,
-} from "./schema/event/background/update-background-name-event-schema";
+} from "./schema/event-schema/background/update-background-name-event-schema";
 export {
   updateBackgroundSvgEventSchema,
   UpdateBackgroundSvgEventSchema,
-} from "./schema/event/background/update-background-svg-event-schema";
+} from "./schema/event-schema/background/update-background-svg-event-schema";
 export {
   createCharacterEventSchema,
   CreateCharacterEventSchema,
-} from "./schema/event/character/create-character-event-schema";
+} from "./schema/event-schema/character/create-character-event-schema";
 export {
   deleteCharacterEventSchema,
   DeleteCharacterEventSchema,
-} from "./schema/event/character/delete-character-event-schema";
+} from "./schema/event-schema/character/delete-character-event-schema";
 export {
   updateCharacterNameEventSchema,
   UpdateCharacterNameEventSchema,
-} from "./schema/event/character/update-character-name-event-schema";
+} from "./schema/event-schema/character/update-character-name-event-schema";
 export {
   createEmoteEventSchema,
   CreateEmoteEventSchema,
-} from "./schema/event/emote/create-emote-event-schema";
+} from "./schema/event-schema/emote/create-emote-event-schema";
 export {
   deleteEmoteEventSchema,
   DeleteEmoteEventSchema,
-} from "./schema/event/emote/delete-emote-event-schema";
+} from "./schema/event-schema/emote/delete-emote-event-schema";
 export {
   updateEmoteNameEventSchema,
   UpdateEmoteNameEventSchema,
-} from "./schema/event/emote/update-emote-name-event-schema";
+} from "./schema/event-schema/emote/update-emote-name-event-schema";
 export {
   updateEmoteSvgEventSchema,
   UpdateEmoteSvgEventSchema,
-} from "./schema/event/emote/update-emote-svg-event-schema";
+} from "./schema/event-schema/emote/update-emote-svg-event-schema";
 export {
   createSceneEventSchema,
   CreateSceneEventSchema,
-} from "./schema/event/scene/create-scene-event-schema";
+} from "./schema/event-schema/scene/create-scene-event-schema";
 export {
   deleteSceneEventSchema,
   DeleteSceneEventSchema,
-} from "./schema/event/scene/delete-scene-event-schema";
+} from "./schema/event-schema/scene/delete-scene-event-schema";
 export {
   updateSceneBackgroundEventSchema,
   UpdateSceneBackgroundEventSchema,
-} from "./schema/event/scene/update-scene-background-event-schema";
+} from "./schema/event-schema/scene/update-scene-background-event-schema";
 export {
   updateSceneNameEventSchema,
   UpdateSceneNameEventSchema,
-} from "./schema/event/scene/update-scene-name-event-schema";
-export { eventSchema, EventSchema } from "./schema/event";
+} from "./schema/event-schema/scene/update-scene-name-event-schema";
+export { eventSchema, EventSchema } from "./schema/event-schema";
 export { nameSchema, NameSchema } from "./schema/name-schema";
 export { svgSchema, SvgSchema } from "./schema/svg-schema";
 export { uuidSchema, UuidSchema } from "./schema/uuid-schema";
