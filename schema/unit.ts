52493f2060979f35c122791b1a3986b3a511a4fa
jameswilddev <jameswilddev@outlook.com>
2020-05-30 12:52:19 +0100

    Refactor tests.

--- schema/unit.ts
+++ schema/unit.ts
@@ -281,3 +281,207 @@ export function rejectsOtherThanExpectedString(
     );
   });
 }
+
+export function validateUnpaddedString(
+  description: string,
+  schema: jsonschema.Schema,
+  path: string,
+  length: number,
+  overriddenErrors: null | ReadonlyArray<string>,
+  instanceFactory: (name: Json) => Json
+): void {
+  describe(description, () => {
+    accepts(`single character`, instanceFactory(`T`), schema);
+
+    rejects(
+      `single white space character`,
+      instanceFactory(` `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `single character with preceding white space`,
+      instanceFactory(` T`),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `single character with trailing white space`,
+      instanceFactory(`T `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    accepts(`two characters`, instanceFactory(`Te`), schema);
+
+    rejects(
+      `two white space characters`,
+      instanceFactory(`  `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `two characters with preceding white space`,
+      instanceFactory(` Te`),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `two characters with trailing white space`,
+      instanceFactory(`Te `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    accepts(`three characters`, instanceFactory(`Tes`), schema);
+    accepts(
+      `three characters containing white space`,
+      instanceFactory(`T s`),
+      schema
+    );
+
+    rejects(
+      `three white space characters`,
+      instanceFactory(`   `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `three characters with preceding white space`,
+      instanceFactory(` Tes`),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `three characters with trailing white space`,
+      instanceFactory(`Tes `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    accepts(`many characters`, instanceFactory(`Test Valid Name`), schema);
+
+    rejects(
+      `many characters with preceding white space`,
+      instanceFactory(` Test Valid Name`),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `many characters with trailing white space`,
+      instanceFactory(`Test Valid Name `),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    accepts(`the length limit`, instanceFactory(`T`.repeat(length)), schema);
+
+    rejects(
+      `beyond the length limit`,
+      instanceFactory(`T`.repeat(length + 1)),
+      schema,
+      overriddenErrors || [`${path} does not meet maximum length of 50`]
+    );
+
+    rejects(
+      `null`,
+      instanceFactory(null),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `empty strings`,
+      instanceFactory(``),
+      schema,
+      overriddenErrors || [
+        `${path} does not match pattern "^\\\\S(?:.*\\\\S)?$"`,
+      ]
+    );
+
+    rejects(
+      `zero`,
+      instanceFactory(0),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `negative zero`,
+      instanceFactory(-0),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `positive integers`,
+      instanceFactory(326),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `negative integers`,
+      instanceFactory(-326),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `positive decimals`,
+      instanceFactory(32.6),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `negative decimals`,
+      instanceFactory(-32.6),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `empty arrays`,
+      instanceFactory([]),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+
+    rejects(
+      `empty objects`,
+      instanceFactory({}),
+      schema,
+      overriddenErrors || [`${path} is not of a type(s) string`]
+    );
+  });
+}
