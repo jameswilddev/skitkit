4da811ba64a3f69740a04e2d728530e32ff23d66
jameswilddev <jameswilddev@outlook.com>
2020-05-31 13:42:18 +0100

    Refactor UUID validation tests.

--- schema/uuid-schema/unit.ts
+++ schema/uuid-schema/unit.ts
@@ -2,6 +2,32 @@ import * as jsonschema from "jsonschema";
 import { accepts, rejects } from "../unit";
 import { Json, uuidSchema } from "../..";
 
+export function forEachInvalidUuid(
+  callback: (description: string, value: string) => void
+): void {
+  callback(`empty strings`, ``);
+
+  callback(`capitalized uuids`, `A366E69C-D60E-4E27-BD18-7AEA8257BCDB`);
+
+  callback(`unhyphenated uuids`, `a366e69cd60e4e27bd187aea8257bcdb`);
+
+  callback(`braced uuids`, `{a366e69c-d60e-4e27-bd18-7aea8257bcdb}`);
+
+  callback(`extended uuids`, `a366e69c-d60e-4e27-bd18-7aea8257bcdbe`);
+
+  callback(`truncated uuids`, `a366e69c-d60e-4e27-bd18-7aea8257bcd`);
+
+  callback(
+    `uuids with preceding white space`,
+    ` a366e69c-d60e-4e27-bd18-7aea8257bcdb`
+  );
+
+  callback(
+    `uuids with trailing white space`,
+    `a366e69c-d60e-4e27-bd18-7aea8257bcdb `
+  );
+}
+
 export function validateUuidSchema(
   description: string,
   schema: jsonschema.Schema,
@@ -16,83 +42,22 @@ export function validateUuidSchema(
       schema
     );
 
+    forEachInvalidUuid((description, value) => {
+      rejects(
+        description,
+        instanceFactory(value),
+        schema,
+        overriddenErrors || [
+          `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
+        ]
+      );
+    });
+
     rejects(
       `null`,
       instanceFactory(null),
       schema,
       overriddenErrors || [`${path} is not of a type(s) string`]
-    );
-
-    rejects(
-      `empty strings`,
-      instanceFactory(``),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `capitalized uuids`,
-      instanceFactory(`A366E69C-D60E-4E27-BD18-7AEA8257BCDB`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `unhyphenated uuids`,
-      instanceFactory(`a366e69cd60e4e27bd187aea8257bcdb`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `braced uuids`,
-      instanceFactory(`{a366e69c-d60e-4e27-bd18-7aea8257bcdb}`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `extended uuids`,
-      instanceFactory(`a366e69c-d60e-4e27-bd18-7aea8257bcdbe`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `truncated uuids`,
-      instanceFactory(`a366e69c-d60e-4e27-bd18-7aea8257bcd`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `uuids with preceding white space`,
-      instanceFactory(` a366e69c-d60e-4e27-bd18-7aea8257bcdb`),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
-    );
-
-    rejects(
-      `uuids with trailing white space`,
-      instanceFactory(`a366e69c-d60e-4e27-bd18-7aea8257bcdb `),
-      schema,
-      overriddenErrors || [
-        `${path} does not match pattern "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$"`,
-      ]
     );
 
     rejects(
