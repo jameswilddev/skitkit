ba978f4b88d7c7aeed00ec8cfb9cf854384f52db
jameswilddev <jameswilddev@outlook.com>
2020-06-20 17:05:31 +0100

    PersistedSkit -> History.

--- editor/history/index.ts
+++ editor/history/index.ts
@@ -1,7 +1,7 @@
 import { State } from "../../types/state";
 import { EventSchema } from "../../schema/event-schema";
 
-export type PersistedSkit = {
+export type History = {
   readonly beforeSteps: State;
   readonly doneSteps: ReadonlyArray<EventSchema>;
   readonly undoneSteps: ReadonlyArray<EventSchema>;
